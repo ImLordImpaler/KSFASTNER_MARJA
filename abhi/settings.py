afb2e0ee01a28311dfa11130ff3934c0d8d97b1a
ImLordImpaler <40574240+ImLordImpaler@users.noreply.github.com>
2020-11-02 23:16:28 +0530

    Update settings.py

--- abhi/settings.py
+++ abhi/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR , 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'staticfiles/media')
 
 #url should be:
 # https://ksfastners-static.s3.amazonaws.com/static/static/media/media/portfolio-2.jpg
